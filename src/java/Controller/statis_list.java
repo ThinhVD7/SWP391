7ace94f0f9201d0e318bc8d66cc5c68742bd66ef
thinhvd <thinhlclc23@gmail.com>
2023-11-03 08:54:07 +0700

    update list

--- src/java/Controller/statis_list.java
+++ src/java/Controller/statis_list.java
@@ -56,3 +56,117 @@ public class Course {
         this.endDate = endDate;
     }
 }
+
+public Exam(String examID, String classID, String examName, int questionNumber, String startDate, String endDate, String timeLimit, int attempsAllowed, String examDetail, float maxScore, int permission) {
+        this.examID = examID;
+        this.classID = classID;
+        this.examName = examName;
+        this.questionNumber = questionNumber;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.timeLimit = timeLimit;
+        this.attempsAllowed = attempsAllowed;
+        this.examDetail = examDetail;
+        this.maxScore = maxScore;
+        this.permission = permission;
+    }
+
+    public String getExamID() {
+        return examID;
+    }
+
+    public void setExamID(String examID) {
+        this.examID = examID;
+    }
+
+    public String getClassID() {
+        return classID;
+    }
+
+    public void setClassID(String classID) {
+        this.classID = classID;
+    }
+
+    public String getExamName() {
+        return examName;
+    }
+
+    public void setExamName(String examName) {
+        this.examName = examName;
+    }
+
+    public int getQuestionNumber() {
+        return questionNumber;
+    }
+
+    public void setQuestionNumber(int questionNumber) {
+        this.questionNumber = questionNumber;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getTimeLimit() {
+        return timeLimit;
+    }
+
+    public void setTimeLimit(String timeLimit) {
+        this.timeLimit = timeLimit;
+    }
+
+    public int getAttempsAllowed() {
+        return attempsAllowed;
+    }
+ public String getQuestionID() {
+        return questionID;
+    }
+
+    public void setQuestionID(String questionID) {
+        this.questionID = questionID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+     public Class1(String classID, String className, String courseID) {
+        this.classID = classID;
+        this.className = className;
+        this.courseID = courseID;
+    }
+
+    public String getClassID() {
+        return classID;
+    }
+
+    public String getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(String courseID) {
+        this.courseID = courseID;
+    }
--- web/Statis_list.jsp
+++ web/Statis_list.jsp
@@ -61,3 +61,48 @@
             <div class="w3-container w3-black w3-center w3-opacity w3-padding-64">
                 <h1 class="w3-margin w3-xlarge">Quote of the day: live life</h1>
             </div>
+            <footer class="w3-container w3-padding-64 w3-center w3-opacity">  
+                <div class="w3-xlarge w3-padding-32">
+                    <i class="fa fa-facebook-official w3-hover-opacity"></i>
+                    <i class="fa fa-instagram w3-hover-opacity"></i>
+                    <i class="fa fa-snapchat w3-hover-opacity"></i>
+                    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+                    <i class="fa fa-twitter w3-hover-opacity"></i>
+                    <i class="fa fa-linkedin w3-hover-opacity"></i>
+                </div>
+                <p>Sign in  <a href="Login">here</a></p>
+            </footer>
+    
+            <script>
+                // Used to toggle the menu on small screens when clicking on the menu button
+                function myFunction() {
+                    var x = document.getElementById("navDemo");
+                    if (x.className.indexOf("w3-show") == -1) {
+                        x.className += " w3-show";
+                    } else {
+                        x.className = x.className.replace(" w3-show", "");
+                    }
+                }
+            
+    /* Track */
+    ::-webkit-scrollbar-track {
+      background: #f1f1f1; 
+    }
+
+    /* Handle */
+    ::-webkit-scrollbar-thumb {
+      background: #888; 
+    }
+
+    /* Handle on hover */
+    ::-webkit-scrollbar-thumb:hover {
+      background: #555; 
+    } /* Importing fonts from Google */
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap');
+
+/* Reseting */
+* 
+            </script>
+    
+        </body>
+    </html>
