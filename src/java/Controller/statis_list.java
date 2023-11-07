b688e10e07dcfa4848e6986b5fe0100d0c585e7d
thinhvd <thinhlclc23@gmail.com>
2023-11-07 22:02:28 +0700

    thinhvd

--- src/java/Controller/statis_list.java
+++ src/java/Controller/statis_list.java
@@ -349,84 +349,49 @@ protected void processRequest(HttpServletRequest request, HttpServletResponse re
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-}
-protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet studentHome</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet studentHome at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
  @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException 
-    {
-        //block check if user have logged in, if not then return to index
+            throws ServletException, IOException {
+        //block check if user have logged in, if true then return to home
         HttpSession session = request.getSession(false);
-        if(session == null||session.getAttribute("user") == null)
+        if(session ==null)
+            request.getRequestDispatcher("Login.jsp").forward(request, response);
+        if(session.getAttribute("user") != null)
         {
-            response.sendRedirect("index.html");
+            response.sendRedirect("home");
             return;
         }
         ////////////////////////////////////////////////////////////////
-        Account user = (Account)session.getAttribute("user");
-        //check user's authority by role
-        if(user.getRoleID()!=3)
-            request.getRequestDispatcher("pageNotFound").forward(request, response);
-        ///////////////////////////////
-        DAO dao = new DAO();
-////test session block//////////////////////////////////////////////////////////////
-//try (PrintWriter out = response.getWriter()) {
-///* TODO output your page here. You may use following sample code. */
-//out.println("<!DOCTYPE html>");
-//out.println("<html>");
-//out.println("<head>");
-//out.println("<title>Servlet DemoSession03</title>");  
-//out.println("</head>");
-//out.println("<body>");
-//out.print("<h1>SessionId: "+session.getId()+ "</h1>");
-//Enumeration enu = session.getAttributeNames();
-//while(enu.hasMoreElements())
-//{
-//    String key = enu.nextElement() + "";
-//    Object value = session.getAttribute(key);
-//    out.print("<h1> Attribute name = "+key+" : value = "+value);
-//    out.print("<h2> Object:" +((Account)session.getAttribute("user")).getEmail()
-//            +"role: " +((Account)session.getAttribute("user")).getRoleID()
-//            +"</h2>");
-//}
-//out.println("</body>");
-//out.println("</html>");
-//}
-////////////////////////////////////////////////////////////////////////////////////
-        LecturerDAO daoTemp = new LecturerDAO();
-        List<Class1> userClasses = dao.getClass(user.accountID);
-        request.setAttribute("courseInfo", daoTemp.loadAllStudentCourse(user.accountID));
-        request.setAttribute("classes", userClasses);
-        request.getRequestDispatcher("student-homepage.jsp").forward(request, response);
-
+        request.getRequestDispatcher("Login.jsp").forward(request, response);
     }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        HttpSession session = request.getSession();
+        
+        //block check if user have logged in, if true then return to home
+        if(session.getAttribute("user") != null)
+        {
+            Account userCheck = (Account)session.getAttribute("user");
+            if(userCheck.getStatus()==0)
+                {
+                    session.removeAttribute("user");
+                    request.setAttribute("mess", "Your account has been suspended. Be nicer next time!");
+                    request.getRequestDispatcher("Login.jsp").forward(request, response);
+                }
+            response.sendRedirect("home");
+            return;
+        }
+        ////////////
